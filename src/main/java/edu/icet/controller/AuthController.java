3c5a5272aebabcb7f7c0f62c3ed5ffc08765aba0
gihani571 <gihanimadhubashini75@gmail.com>
2025-04-14 11:14:58 +0530

    jw Authentication

---
+++ src/main/java/edu/icet/controller/AuthController.java
@@ -0,0 +1,32 @@
+package edu.icet.controller;
+
+import edu.icet.dto.JwtAuthResponse;
+import edu.icet.dto.Login;
+import edu.icet.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private AuthService authService;
+
+    // Build Login REST API
+    @PostMapping("/login")
+    public ResponseEntity<JwtAuthResponse> login(@RequestBody Login login){
+        String token = authService.login(login);
+
+        JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
+        jwtAuthResponse.setAccessToken(token);
+
+        return new ResponseEntity<>(jwtAuthResponse, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/edu/icet/dto/JwtAuthResponse.java
@@ -0,0 +1,15 @@
+package edu.icet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthResponse {
+
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+}
